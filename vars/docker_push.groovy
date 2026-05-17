83a1854c20ac34bdf8bef00dfc348992fcce41c2
Ritik Bawane <Ritikbawane5437@gmail.com>
2026-05-17 13:16:59 +0530

    Enhance docker_push.groovy for secure login and feedback
    Refactor Docker push function to improve security and logging.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,23 @@
+// vars/docker_push.groovy
+
 def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubcred', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
-      sh "docker login -u ${DOCKER_USER} -p ${dockerHubcred}"
-  }
-  sh "docker push ${DOCKER_USER}/${Project}:${ImageTag}"
+
+    echo "Pushing Docker image to DockerHub"
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerHubcred',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+
+        sh '''
+        echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+        '''
+
+        sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+    }
+
+    echo "Docker image pushed successfully"
 }
